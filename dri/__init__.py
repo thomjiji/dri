83bf97414cab9b2d46806eda09e1cb7739af5bc1
thomjiji <hxy449999@gmail.com>
2023-06-04 11:33:23 +0800

    resolve_init work

--- dri/__init__.py
+++ dri/__init__.py
@@ -1,3 +1,21 @@
+import importlib.util
+
 from .folder import Folder
 from .media_pool_item import MediaPoolItem
 from .media_pool import MediaPool
+
+
+def load_dynamic_lib():
+    spec = importlib.util.spec_from_file_location(
+        "fusionscript",
+        "/Applications/DaVinci Resolve/DaVinci Resolve.app/Contents/Libraries/Fusion/fusionscript.so",
+    )
+    bmd_module = importlib.util.module_from_spec(spec)
+
+    return bmd_module
+
+
+def resolve_init():
+    bmd_module = load_dynamic_lib()
+    resolve = bmd_module.scriptapp("Resolve")
+    return resolve
