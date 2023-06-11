3c947489ad4dee31734fc73a1046d72c4ef6364e
thomjiji <hxy449999@gmail.com>
2023-06-11 22:34:22 +0800

    continue test_resolve.py

--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -61,7 +61,40 @@ class TestResolve(unittest.TestCase):
     @skip_if_resolve_none
     def test_GetVersion(self):
         version = self.resolve.GetVersion()
-        self.assertIsNotNone(version)
+        major = version[0]
+        minor = version[1]
+        patch = version[2]
+        build = version[3]
+        suffix = version[4]
+
+        # Firstly, test version should be a list
+        self.assertIsInstance(version, list, "version should be a list")
+
+        # Test whether the length of the version list is 5
+        self.assertEqual(len(version), 5, "Version should have exactly 5 items")
+
+        # Test individual item's type
+        self.assertIsInstance(major, int, "Major version should be an integer")
+        self.assertIsInstance(minor, int, "Minor version should be an integer")
+        self.assertIsInstance(patch, int, "Patch version should be an integer")
+        self.assertIsInstance(build, int, "Build version should be an integer")
+        self.assertIsInstance(suffix, str, "Suffix should be a string")
+
+    @skip_if_resolve_none
+    def test_GetVersionString(self):
+        version = self.resolve.GetVersionString()
+        self.assertIsInstance(version, str)
+
+    @skip_if_resolve_none
+    def test_LoadLayoutPreset(self):
+        # Precondition and configuration
+        self.resolve.SaveLayoutPreset("test_LoadLayoutPreset")
+
+        result = self.resolve.LoadLayoutPreset("test_LoadLayoutPreset")
+        self.assertTrue(result)
+
+        # Back to the initial state
+        self.resolve.DeleteLayoutPreset("test_LoadLayoutPreset")
 
 
 if __name__ == "__main__":
