f728e6dff79a509efdddffc1d72a929001d4588d
thomjiji <hxy449999@gmail.com>
2023-06-11 14:40:01 +0800

    skip tests if resolve in None

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,6 +1,7 @@
 import unittest
 
 from dri import Resolve
+from tests.test_resolve import skip_if_resolve_none
 
 
 class TestProject(unittest.TestCase):
@@ -8,6 +9,7 @@ class TestProject(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.resolve = Resolve.resolve_init()
 
+    @skip_if_resolve_none
     def test_GetMediaPool(self):
         project_manager = self.resolve.GetProjectManager()
         project = project_manager.GetCurrentProject()
--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -3,6 +3,16 @@ import unittest
 from dri import load_dynamic_lib, Resolve
 
 
+def skip_if_resolve_none(func):
+    def wrapper(self, *args, **kwargs):
+        if self.resolve is None:
+            self.skipTest("resolve object is None, skipping test.")
+        else:
+            return func(self, *args, **kwargs)
+
+    return wrapper
+
+
 class TestLoadDynamicLib(unittest.TestCase):
     def test_load_dynamic_lib(self):
         bmd_module = load_dynamic_lib()
@@ -13,27 +23,26 @@ class TestResolve(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.resolve = Resolve.resolve_init()
-        # print("setup is done")
-
-    # @classmethod
-    # def tearDownClass(cls) -> None:
-    #     print("tear down is taking over")
 
     def test_resolve_init(self):
         self.assertIsNotNone(self.resolve)
 
+    @skip_if_resolve_none
     def test_Fusion(self):
         fusion = self.resolve.Fusion()
         self.assertIsNotNone(fusion)
 
+    @skip_if_resolve_none
     def test_GetMediaStorage(self):
         media_storage = self.resolve.GetMediaStorage()
         self.assertIsNotNone(media_storage)
 
+    @skip_if_resolve_none
     def test_GetProjectManager(self):
         project_manager = self.resolve.GetProjectManager()
         self.assertIsNotNone(project_manager)
 
+    @skip_if_resolve_none
     def test_OpenPage(self):
         # Precondition and configuration
         current_page = self.resolve.GetCurrentPage()
@@ -48,13 +57,20 @@ class TestResolve(unittest.TestCase):
         # Back to the initial state
         self.resolve.OpenPage(current_page)
 
+    @skip_if_resolve_none
     def test_GetCurrentPage(self):
         pages = ["media", "cut", "edit", "fusion", "color", "fairlight", "deliver"]
         self.assertIn(self.resolve.GetCurrentPage(), pages)
 
+    @skip_if_resolve_none
     def test_GetProductName(self):
         product_names = ["DaVinci Resolve Studio", "DaVinci Resolve"]
         self.assertIn(self.resolve.GetProductName(), product_names)
+
+    @skip_if_resolve_none
+    def test_GetVersion(self):
+        version = self.resolve.GetVersion()
+        self.assertIsNotNone(version)
 
 
 if __name__ == "__main__":
