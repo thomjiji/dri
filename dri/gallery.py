1fdfcbb796b41217b4c466d08c18a651bb9f0f7b
thomjiji <hxy449999@gmail.com>
2024-02-05 21:03:22 +0800

    api: Complete gallery
    Signed-off-by: thomjiji <hxy449999@gmail.com>

--- dri/gallery.py
+++ dri/gallery.py
@@ -1,2 +1,76 @@
+from dri.gallery_still_album import GalleryStillAlbum
+
+
 class Gallery:
-    pass
+    def GetAlbumName(self, gallery_still_album: GalleryStillAlbum) -> str:
+        """
+        Returns the name of the :class:`dri.GalleryStillAlbum` object
+        "galleryStillAlbum".
+
+        Parameters
+        ----------
+        gallery_still_album
+            :class:`dri.GalleryStillAlbum` object
+
+        Returns
+        -------
+        str
+            Album name.
+
+        """
+        ...
+
+    def SetAlbumName(
+        self, gallery_still_album: GalleryStillAlbum, album_name: str
+    ) -> bool:
+        """
+        Sets the name of the GalleryStillAlbum object "galleryStillAlbum" to "albumName".
+
+        Parameters
+        ----------
+        gallery_still_album
+            :class:`dri.GalleryStillAlbum` object
+        album_name
+            Album name to set.
+
+        Returns
+        -------
+        bool
+            True if successful, False otherwise.
+
+        """
+        ...
+
+    def GetCurrentStillAlbum(self) -> GalleryStillAlbum:
+        """
+        Returns current album as a GalleryStillAlbum object.
+        """
+        ...
+
+    def SetCurrentStillAlbum(self, gallery_still_album: GalleryStillAlbum) -> bool:
+        """
+        Sets current album to GalleryStillAlbum object "galleryStillAlbum".
+
+        Parameters
+        ----------
+        gallery_still_album
+            :class:`dri.GalleryStillAlbum` object
+
+        Returns
+        -------
+        bool
+            True if successful, False otherwise.
+
+        """
+        ...
+
+    def GetGalleryStillAlbums(self) -> list[GalleryStillAlbum]:
+        """
+        Returns the gallery albums as a list of GalleryStillAlbum objects.
+
+        Returns
+        -------
+        list[GalleryStillAlbum]
+            A list of :class:`dri.GalleryStillAlbum` object.
+        """
+        ...
